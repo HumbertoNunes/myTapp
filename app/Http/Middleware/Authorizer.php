04c5ab1b5c15114f916c30ca7620b941fd11ebc2
Humberto.Souza <humberto.nts@gmail.com>
2019-06-19 11:07:00 -0400

    Adicionando middleware de redirecionamento

---
+++ app/Http/Middleware/Authorizer.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class Authorizer
+{
+
+    public function handle($request, Closure $next)
+    {
+    	if(!$request->is('login') && \Auth::guest()) {
+            return redirect('/login');
+        }
+
+        return $next($request);
+    }
+}
