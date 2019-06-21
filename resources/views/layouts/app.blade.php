59e9b6427731080d3b26704d4a0ec6ccf04a2a33
Humberto <humberto.nts@gmail.com>
2019-06-20 20:58:42 -0400

    Adicionando favicon.svg

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,6 +25,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
+                    <img src="{{ asset('img/beer.svg') }}" width="30" height="30" alt="">
                     MyTapp
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
