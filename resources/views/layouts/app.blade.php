40ed742e160d2efbae998a62be7a64b7af249a0b
Humberto <humberto.nts@gmail.com>
2019-06-21 20:45:01 -0400

    Adicionando feather icons (CDN) e lock/unlock svg

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,7 @@
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/feather-icons/dist/feather.min.js"></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -94,5 +95,6 @@
     </div>
 
     @stack('scripts')
+    <script type="text/javascript">feather.replace();</script>
 </body>
 </html>
